5c7afab6e74d8a29e099d8acaf1c86deb9e4d67b
Claudia Gonzalez <claudiacastanieda@gmail.com>
2025-03-12 00:29:52 -0600

    Ajuste en variables de entorno 7

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,8 +1,9 @@
-export const EnvConfiguration = () => ({
-    environment : process.env.NODE_ENV || 'dev',
-    mongodb : process.env.MONGODB || 'mongodb://mongo:GJrJzSkIXEDMuJpTKJfkddDrvQxvPBrD@centerbeam.proxy.rlwy.net:11110',
-    port : process.env.PORT || 3002,
-    default_limit : process.env.DEFAULT_LIMIT || 7,
-
-
-})
+export const EnvConfiguration = () => {
+    console.log('MONGODB Connection String:', process.env.MONGODB);
+    return {
+      environment : process.env.NODE_ENV || 'dev',
+      mongodb : process.env.MONGODB || 'mongodb://mongo:GJrJzSkIXEDMuJpTKJfkddDrvQxvPBrD@centerbeam.proxy.rlwy.net:11110',
+      port : process.env.PORT || 3002,
+      default_limit : process.env.DEFAULT_LIMIT || 7,
+    };
+  };
--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object ({
-    MONGODB: Joi.required(),
+    MONGODB: Joi.string().uri().default('mongodb://mongo:GJrJzSkIXEDMuJpTKJfkddDrvQxvPBrD@centerbeam.proxy.rlwy.net:11110').optional(),
     PORT: Joi.number().default(3000),
     DEFAULT_LIMIT: Joi.number().default(6)
 })
