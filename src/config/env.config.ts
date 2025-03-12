6c9bb1f784f3943bbd306fc613f833e9e911c516
Claudia Gonzalez <claudiacastanieda@gmail.com>
2025-03-12 00:19:10 -0600

    Ajuste en variables de entorno 5

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 export const EnvConfiguration = () => ({
     environment : process.env.NODE_ENV || 'dev',
-    mongodb : process.env.MONGODB,
+    mongodb : process.env.MONGODB || 'mongodb://mongo:GJrJzSkIXEDMuJpTKJfkddDrvQxvPBrD@centerbeam.proxy.rlwy.net:11110',
     port : process.env.PORT || 3002,
     default_limit : process.env.DEFAULT_LIMIT || 7,
 
